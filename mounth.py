3227fe60af5cc6847fa1dfaa528c20193684c085
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2021-10-06 08:59:43 -0700

    Create mounth.py

---
+++ mounth.py
@@ -0,0 +1,32 @@
+aa = int(input())
+if (1 == a ):
+    print("January")
+elif (2 == a ):
+    print("February")
+elif (3 == a ):
+    print("March")
+elif (4 == a ):
+    print("April")
+elif (5 == a ):
+    print("May")
+elif (6 == a ):
+    print("June")
+elif (7 == a ):
+    print("July")
+elif (8 == a ):
+    print("August")
+elif (9 == a ):
+    print("September")
+elif (10 == a ):
+    print("October")
+elif (11 == a ):
+    print("November")
+elif (12 == a ):
+    print("December")
+    
+_________________
+output
+6 
+June
+
+limk: https://www.urionlinejudge.com.br/judge/en/runs/code/24785664
