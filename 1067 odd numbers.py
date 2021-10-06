0653b519d23d34e324a10fe303aa4e497d4e3c06
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2021-10-06 09:05:37 -0700

    Create 1067 odd numbers.py

---
+++ 1067 odd numbers.py
@@ -0,0 +1,15 @@
+a = 1
+x =int(input())
+while(a <= x):
+    
+    print(a)
+    a += 2
+    
+_________________________
+output
+6
+1
+3
+5
+
+link: https://www.urionlinejudge.com.br/judge/en/runs/code/24785335
