83a2de115183277d2b4d62d6d52eb6dc3c2c8dc0
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2021-10-05 23:57:52 -0700

    Create problemuribeginer.py

---
+++ DDD/problemuribeginer.py
@@ -0,0 +1,28 @@
+a = int(input())
+if (61 == a):
+    print("Brasilia")
+elif (71 == a):
+    print("Salvador")
+elif (11 == a):
+    print("Sao Paulo")
+elif (21 == a):
+    print("Rio de Janeiro")
+elif (32 == a):
+    print("Juiz de Fora")
+elif (19 == a):
+    print("Campinas")
+elif (27 == a):
+    print("Vitoria")
+elif (31 == a):
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
+__________________________________________________
+output
+61                   Brasilia
+
+______
+214
+DDD nao cadastrado
+
+link___https://www.urionlinejudge.com.br/judge/en/runs/code/24780427
