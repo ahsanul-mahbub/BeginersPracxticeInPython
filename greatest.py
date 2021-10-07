4d05632033de838ac46b8295e5bd6311ad77afa4
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2021-10-07 07:19:37 -0700

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,11 @@
+a,b,c= map(int, input().split())
+print(max(a,b,c),"eh o maior")
+
+
+_________________________________
+output
+23 45 21
+45 eh o maior 
+
+
+limk : https://www.urionlinejudge.com.br/judge/en/problems/view/1016
