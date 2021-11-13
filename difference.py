36478a4e9c3ca8c33b61c45bf557baa2b0875485
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2021-11-13 23:14:51 +0600

    Create difference.py

---
+++ difference.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+dif =(a*b)-(c*d)
+print("DIFERENCA =",dif)
+
+
+
+___________________________
+5
+6
+7
+8
+DIFERENCA = -26
+
+
+___________________________
+
+https://www.beecrowd.com.br/judge/en/problems/view/1007
