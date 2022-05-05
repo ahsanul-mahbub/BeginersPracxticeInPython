8c47db1f93084f4c94fc9678a1298a05b61f2e55
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2022-05-06 02:41:59 +0600

    Create rangefunction.py

---
+++ rangefunction.py
@@ -0,0 +1,19 @@
+a = list(range(10))
+print(a)
+print(a[2])
+
+
+a = list(range(5,11))
+print(a)
+
+a = list(range(2,101,2))
+print(a)
+
+
+
+
+
+____________________
+output
+[0,1,2,3,4,5,6,7,8,9]
+[5,6,7,8,9,10]
