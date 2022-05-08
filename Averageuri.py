a065c31f6feb19503934668b306acaa5f6318308
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2022-05-09 04:03:37 +0600

    Create Averageuri.py

---
+++ Averageuri.py
@@ -0,0 +1,24 @@
+a = float(input())
+b = float(input())
+e = float(input())
+a = a * 2
+b = b * 3
+e = e * 5
+c = a + b + e
+d = 2 + 3 + 5
+c = c / d
+
+,,,
+_________________
+Output
+5.0
+6.0
+7.0
+
+6.3
+
+
+
+print("MEDIA = %.1f" %c)https://www.beecrowd.com.br/judge/en/problems/view/1006
+
+,,,
