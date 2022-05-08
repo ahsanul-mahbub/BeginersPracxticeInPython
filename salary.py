30ac1f02d8ebc85c8a9aefdee4f6ef265958a370
Farhan <89720951+Farhanchokder@users.noreply.github.com>
2022-05-09 03:38:21 +0600

    Create salary.py

---
+++ salary.py
@@ -0,0 +1,23 @@
+a = int(input())
+b = int(input())
+c = float(input())
+print("NUMBER =",a)
+d = c * b
+print("SALARY = U$",'%.2f'%d)
+
+
+,,,
+___________________
+Output
+25    
+100              
+5.50
+
+NUMBER = 25
+SALARY = U$ 550.00
+
+
+
+
+https://www.beecrowd.com.br/judge/en/runs/code/27813039
+,,,
